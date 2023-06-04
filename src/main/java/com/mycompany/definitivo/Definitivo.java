13ad538c1d17b632d41916d120fe2da6df56112f
Nuria <Nuria@DESKTOP-AE95HB6>
2023-06-04 17:33:54 +0100

    he arreglado el fallo que había en el main

---
+++ src/main/java/com/mycompany/definitivo/Definitivo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.definitivo;
+
+/**
+ *
+ * @author Nuria
+ */
+public class Definitivo {
+
+    public static void main(String[] args) {
+      String frase="Hoy es un estupendo dia para aprender a programar en Java";
+       
+        String frase_resumen=frase.substring(0,3);
+    }
+}
---
+++ src/main/java/com/mycompany/definitivo/Saludo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.definitivo;
+
+/**
+ *
+ * @author Nuria
+ */
+public class Saludo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        System.out.println("Hola Mundo");
+    }
+    
+}
